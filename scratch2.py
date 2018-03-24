77e869b46d0124db079f351196a8e439005b2036
florianwechsung <f.wechsung@gmail.com>
2018-03-24 15:03:24 +0000

    Progress! Gives the right result for Coefficients (i.e. things that move with the mesh) but not the right thing for expressions (things that don't move). For \int f dx, currently the shape derivative that is computed is \int f \div w dx, but it should be \int \div(fw) dx

--- scratch2.py
+++ scratch2.py
@@ -1,15 +1,23 @@
 from firedrake import *
 import ufl
-mesh = UnitSquareMesh(10, 10)
+mesh = UnitSquareMesh(3, 3)
+# mesh = UnitIntervalMesh(4)
 V = FunctionSpace(mesh, "CG", 1)
 
 u = Function(V)
+(x, y) = SpatialCoordinate(mesh)
+u.interpolate(x)
 
-u.interpolate(Constant(1.0))
+w = TestFunction(mesh.coordinates.function_space())
 
 J = inner(grad(u), grad(u)) * dx
+deriv = -2*inner(dot(grad(w), grad(u)), grad(u)) * dx + inner(grad(u), grad(u)) * div(w) * dx
 # J = u * dx
-# J = grad(u)[0] * dx
+# deriv = div(w) * u * dx
+# f = sin(x)
+# J = f * dx
+# deriv = div(f*w) * dx
+# deriv = f*div(w) * dx
 from ufl.algorithms.compute_form_data import *
 form = J
 form = apply_algebra_lowering(form)
@@ -24,7 +32,6 @@ form = apply_geometry_lowering(form)
 form = apply_derivatives(form)
 form = ufl.replace(form, {SpatialCoordinate(mesh): ufl.classes.ReferenceValue(mesh.coordinates)})
 print("form %s" % (form))
-w = TestFunction(mesh.coordinates.function_space())
 d = derivative(form, mesh.coordinates, ufl.classes.ReferenceValue(w))
 shape_derivative = apply_derivatives(d)
 print("-----------")
@@ -32,4 +39,8 @@ print("Shape derivative: ", shape_derivative)
 print("State: ", u)
 print("Coordinate: ",  mesh.coordinates)
 print("Direction: ", w)
-assemble(shape_derivative)
+shape_derivative._cache['shape'] = True
+computed = assemble(shape_derivative).vector()[:]
+actual = assemble(deriv).vector()[:]
+import numpy as np
+print("Error:", np.linalg.norm(computed-actual)/np.linalg.norm(actual))
